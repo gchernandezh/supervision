eb793b004056335f5ed6c5e126a4024b95fa0e8b
gchernandezh <50496066+gchernandezh@users.noreply.github.com>
2026-03-19 10:46:13 -0500

    Update DATABASES settings to include SSL options
    Add SSL options to the database configuration.

--- config/settings.py
+++ config/settings.py
@@ -25,15 +25,18 @@ TEMPLATES=[{
 'APP_DIRS':True,
 }]
 
-DATABASES={
-'default':{
-'ENGINE':'django.db.backends.postgresql',
-'NAME':os.getenv("DB_NAME"),
-'USER':os.getenv("DB_USER"),
-'PASSWORD':os.getenv("DB_PASSWORD"),
-'HOST':os.getenv("DB_HOST"),
-'PORT':'5432'
-}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': '5432',
+        'OPTIONS': {
+            'sslmode': 'require',
+        }
+    }
 }
 
 STATIC_URL='/static/'
